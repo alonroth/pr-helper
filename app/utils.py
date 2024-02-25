fa023580ad1852b7b0ed262e12ed2867eb86b460
Alon Roth <aroth@lightricks.com>
2024-02-25 11:44:29 +0200

    change sha1 to sha256

--- app/utils.py
+++ app/utils.py
@@ -6,5 +6,5 @@ def github_verify_signature(github_app_secret: str, x_hub_signature: str,
                             data: bytes) -> bool:
     # Use HMAC to compute the hash
     hmac_gen = hmac.new(github_app_secret.encode(), data, hashlib.sha1)
-    expected_signature = 'sha1=' + hmac_gen.hexdigest()
+    expected_signature = 'sha256=' + hmac_gen.hexdigest()
     return hmac.compare_digest(expected_signature, x_hub_signature)
